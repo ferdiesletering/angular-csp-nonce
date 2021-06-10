8d0dd767e833dc4e6060aa6b68c2fae07d058575
Ferdie Sletering <s_ferdie@hotmail.com>
2021-06-10 15:56:50 +0200

    add CSP nonce header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, ɵDomSharedStylesHost } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppComponent } from './app.component';
+import { CustomDomSharedStylesHost } from './shared_styles_host';
+import { MatSliderModule } from '@angular/material/slider';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TooltipModule.forRoot(),
+    NoopAnimationsModule,
+    MatSliderModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: 'cspMetaSelector', useValue: 'meta[name="CSP-NONCE"]' },
+    { provide: ɵDomSharedStylesHost, useClass: CustomDomSharedStylesHost },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared_styles_host.ts
@@ -0,0 +1,81 @@
+import { DOCUMENT, ɵgetDOM as getDOM } from '@angular/common';
+import { Inject, Injectable, OnDestroy } from '@angular/core';
+import { ɵSharedStylesHost } from '@angular/platform-browser';
+
+@Injectable()
+export class CustomDomSharedStylesHost
+  extends ɵSharedStylesHost
+  implements OnDestroy
+{
+  // Maps all registered host nodes to a list of style nodes that have been added to the host node.
+  private _hostNodes = new Map<Node, Node[]>();
+  private _nonce: string | null | undefined = null;
+
+  constructor(
+    @Inject(DOCUMENT) private _doc: any,
+    @Inject('cspMetaSelector')
+    private _metaCSPTag: string
+  ) {
+    super();
+    this._hostNodes.set(_doc.head, []);
+    this._setCSPNonce();
+  }
+
+  private _addStylesToHost(
+    styles: Set<string>,
+    host: Node,
+    styleNodes: Node[]
+  ): void {
+    styles.forEach((style: string) => {
+      const styleEl = this._doc.createElement('style');
+      styleEl.textContent = style;
+
+      if (this._nonce) {
+        styleEl.setAttribute('nonce', this._nonce);
+      }
+
+      styleNodes.push(host.appendChild(styleEl));
+    });
+
+    if (this._nonce) {
+      this._removeCSPNonceHeader();
+    }
+  }
+
+  private _setCSPNonce(): void {
+    this._nonce = document
+      .querySelector(this._metaCSPTag)
+      ?.getAttribute('content');
+  }
+
+  private _removeCSPNonceHeader(): void {
+    document.querySelector(this._metaCSPTag)?.remove();
+  }
+
+  addHost(hostNode: Node): void {
+    const styleNodes: Node[] = [];
+    this._hostNodes.set(hostNode, styleNodes);
+  }
+
+  removeHost(hostNode: Node): void {
+    const styleNodes = this._hostNodes.get(hostNode);
+    if (styleNodes) {
+      styleNodes.forEach(removeStyle);
+    }
+    this._hostNodes.delete(hostNode);
+  }
+
+  onStylesAdded(additions: Set<string>): void {
+    this._hostNodes.forEach((styleNodes, hostNode) => {
+      this._addStylesToHost(additions, hostNode, styleNodes);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._hostNodes.forEach((styleNodes) => styleNodes.forEach(removeStyle));
+  }
+}
+
+function removeStyle(styleNode: Node): void {
+  getDOM().remove(styleNode);
+}
